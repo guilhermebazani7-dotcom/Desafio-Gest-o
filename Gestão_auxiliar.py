f18613feeb94b064ea2d9ca0f52d15eea26a71c6
GuilhermeB <guilhermebazani7@gmail.com>
2025-10-29 12:29:43 -0300

    Corrigida com finalização simultânea

--- Gestão_auxiliar.py
+++ Gestão_auxiliar.py
@@ -1,21 +1,23 @@
 def utilidade(escolha):
-    if escolha == '1':
-        Valor_saida = input("Valor gasto: ")
-        data = input("Data da transação:")
-        cat = input("Que tipo de transação:")
-        ws.append([Valor_saida, data, cat])
-        wb.save("GerenGast.xlsx")
+    while True:
+        if escolha == '1':
+            Valor_saida = input("Valor gasto: ")
+            data = input("Data da transação:")
+            cat = input("Que tipo de transação:")
+            ws.append([Valor_saida, data, cat])
+            wb.save("GerenGast.xlsx")
 
-    elif escolha == '2':
-        print("--------------------------------------------------")
-        for linha in ws.iter_rows(min_row=2, values_only=True):
-            Valor_saida, data, cat = linha
-            print(f"No dia {data}, foi feito um gasto de {Valor_saida}, em {cat}")
-            continue
+        elif escolha == '2':
+            print("--------------------------------------------------")
+            for linha in ws.iter_rows(min_row=2, values_only=True):
+                Valor_saida, data, cat = linha
+                print(f"No dia {data}, foi feito um gasto de {Valor_saida}, em {cat}")
+                continue
 
-    elif escolha == '3':
-        print("Salvando... Obrigado por utilizar")
-        break
+        elif escolha == '3':
+            print("Salvando... Obrigado por utilizar")
+            return "quebra"
+            break
 
-    else:
-        print("Opção inválida, tente novamente.")
+        else:
+            print("Opção inválida, tente novamente.")
